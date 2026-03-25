58d52732c68f1c06b3800618be6f21be1ad327e3
Ignacio <ignacio.tello@utp.ac.pa>
2026-03-24 23:06:08 -0500

    Upd2

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 # Importar módulos
 from modules import (intro, eda, insights, conclusions, Panorama, modeling, presentacion
-        , A_1_Ocurrencia, A_1_2_Duracion,A_1_3_MU,A_1_4_Temporal, A_1_5_Clima, A_1_6_MapaCalor
-        , B_2_1_Severidad, B_2_2_Severidad_Prom, B_2_3_Severidad_Ocurr
-        , C_3_1_Modelo_Comparacion, C_3_2_Simulacion
+        # , A_1_Ocurrencia, A_1_2_Duracion,A_1_3_MU,A_1_4_Temporal, A_1_5_Clima, A_1_6_MapaCalor
+        # , B_2_1_Severidad, B_2_2_Severidad_Prom, B_2_3_Severidad_Ocurr
+        # , C_3_1_Modelo_Comparacion, C_3_2_Simulacion
         )
 st.set_page_config(
     page_title="Proyecto Final - Analítica de Accidentes",
@@ -51,29 +51,29 @@ if section == "Presentación":
 
 elif section == "Introducción":
     intro.show()
-elif section == "A 1 Ocurrencia":
-    A_1_Ocurrencia.show()
-elif section == "A 2 Duración":
-    A_1_2_Duracion.show()
-elif section == "A 3 MUTCD":
-    A_1_3_MU.show()
-elif section == "A 4 Analisis Temporal":
-    A_1_4_Temporal.show()
-elif section == "A 5 Clima":
-    A_1_5_Clima.show()
-elif section == "A 6 Mapa de Calor":
-    A_1_6_MapaCalor.show()
-elif section == "B 1 Severidad":
-    B_2_1_Severidad.show()
-elif section == "B 2 Severidad Promedio":
-    B_2_2_Severidad_Prom.show()
-elif section == "B 3 Severidad vs Ocurrencia":
-    B_2_3_Severidad_Ocurr.show()
-elif section == "C 1 Comparación de Modelos":
-    C_3_1_Modelo_Comparacion.show()
+# elif section == "A 1 Ocurrencia":
+#     A_1_Ocurrencia.show()
+# elif section == "A 2 Duración":
+#     A_1_2_Duracion.show()
+# elif section == "A 3 MUTCD":
+#     A_1_3_MU.show()
+# elif section == "A 4 Analisis Temporal":
+#     A_1_4_Temporal.show()
+# elif section == "A 5 Clima":
+#     A_1_5_Clima.show()
+# elif section == "A 6 Mapa de Calor":
+#     A_1_6_MapaCalor.show()
+# elif section == "B 1 Severidad":
+#     B_2_1_Severidad.show()
+# elif section == "B 2 Severidad Promedio":
+#     B_2_2_Severidad_Prom.show()
+# elif section == "B 3 Severidad vs Ocurrencia":
+#     B_2_3_Severidad_Ocurr.show()
+# elif section == "C 1 Comparación de Modelos":
+#     C_3_1_Modelo_Comparacion.show()
 
-elif section == "C 2 Simulación":
-    C_3_2_Simulacion.show()
+# elif section == "C 2 Simulación":
+#     C_3_2_Simulacion.show()
 
 elif section == "Panorama General":
     Panorama.show()
